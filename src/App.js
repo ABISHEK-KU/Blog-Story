39d6253df7151ddbc23eeef22b7e5f7c5b4056a5
Abishek Kumar <87654898+ABISHEK-KU@users.noreply.github.com>
2022-06-19 02:18:27 +0000

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import SearchBar from './Components/SearchBar';
 import StoryCreate from './Components/StoryCreate';
 
 const App=(props)=> {
   return (
     <div>
+      <SearchBar/>
       <StoryCreate/>
     </div>
   );
--- src/Components/StoryCreate.js
+++ src/Components/StoryCreate.js
@@ -4,13 +4,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 
 const StoryCreate = (props) => {
     return (
-        <div className="mainSearchContainer">
-            <div className="searchContainer">
-                <i className="bi bi-search"></i>
-                <input className="searchBox" type="search" name="search" placeholder="Search..." />
-                <i className="bi bi-x-circle"></i>
-            </div>
-        </div>
+        <h1>StoryCreate</h1>
     )
 }
 export default StoryCreate
