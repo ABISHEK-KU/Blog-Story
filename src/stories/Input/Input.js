b55764ee0de06983459c59ffc701a60dec90bbda
Abishek Kumar <87654898+ABISHEK-KU@users.noreply.github.com>
2022-06-19 09:33:52 +0000

    initial commit

--- src/stories/Input/Input.js
+++ src/stories/Input/Input.js
@@ -1,11 +1,16 @@
 import React from "react";
 import './Input.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
-function Input (props){
-    const {size='medium', ...rest} =props
+function Input(props) {
+    const { size = 'medium', ...rest } = props
 
     return (
-        <input className={`input ${size}`} {...rest}/>
+        <div className={`mainSearchContainer ${size}`}>
+            <i className={`bi bi-search ${size}`}></i>
+            <input className={`input`} {...rest} placeholder="Search..." />
+            <i className={`bi bi-x-circle ${size}`}></i>
+        </div>
     )
 }
 
--- src/stories/Input/Input.stories.js
+++ src/stories/Input/Input.stories.js
@@ -6,6 +6,6 @@ export default {
     component:Input
 }
 
-export const Small = () => <Input size='small' placeholder= 'Small Size' />
-export const Medium = () => <Input size='medium' placeholder= 'Medium Size' />
-export const Large = () => <Input size='large' placeholder= 'Large Size' />
+export const Small = () => <Input size='small'/>
+export const Medium = () => <Input size='medium' />
+export const Large = () => <Input size='large'/>
