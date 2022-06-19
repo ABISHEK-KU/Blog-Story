ed0da036642655286a9ecf353d01913c32e5de46
Abishek Kumar <87654898+ABISHEK-KU@users.noreply.github.com>
2022-06-19 17:55:41 +0000

    initial commit

--- src/stories/MarkDownEditor/MarkDownEditor.js
+++ src/stories/MarkDownEditor/MarkDownEditor.js
@@ -1,17 +1,23 @@
 import React,{useState} from "react";
 import { ReactMarkdown } from "react-markdown";
+import './MarkDownEditor.css'
 
 function MarkDownEditor(props){
     const [markdown,setMarkDown]=useState('**markdown')
 
+    const { size = 'medium', rest } = props
+
     const handelChange=(e)=>{
         setMarkDown(e.target.value)
     }
+    
     return(
-        <div className="markdown__container">
+        <div className={`markdown__container ${size}`}>
             <textarea
             value={markdown} onChange={handelChange}/>
             <ReactMarkdown source={markdown} className='markdown__preview'/>
         </div>
     )
 }
+
+export default MarkDownEditor
