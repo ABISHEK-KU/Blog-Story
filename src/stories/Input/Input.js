b163061585cea86bd7e76b59bb6ec8bce7aa3906
Abishek Kumar <87654898+ABISHEK-KU@users.noreply.github.com>
2022-06-19 10:48:26 +0000

    initial commit

--- src/stories/Input/Input.js
+++ src/stories/Input/Input.js
@@ -3,13 +3,13 @@ import './Input.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
 function Input(props) {
-    const { size = 'medium', ...rest } = props
+    const { size = 'medium', searchClick, cancelClick, searchChange ,rest } = props
 
     return (
-        <div className={`mainSearchContainer ${size}`}>
-            <i className={`bi bi-search ${size}`}></i>
-            <input className={`input`} {...rest} placeholder="Search..." />
-            <i className={`bi bi-x-circle ${size}`}></i>
+        <div className={`searchContainer -${size}`}>
+            <i className={`bi bi-search ${size}`} onClick={searchClick}></i>
+            <input className={`input-${size}`} {...rest} placeholder="Search..." onChange={searchChange}/>
+            <i className={`bi bi-x-circle ${size}`} onClick={cancelClick}></i>
         </div>
     )
 }
