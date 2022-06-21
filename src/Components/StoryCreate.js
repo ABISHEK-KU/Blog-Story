ed1aa5aa776415f30812e0952a64840b49ae205b
Abishek Kumar <87654898+ABISHEK-KU@users.noreply.github.com>
2022-06-21 04:29:59 +0000

    initial commit

--- src/Components/StoryCreate.js
+++ src/Components/StoryCreate.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useEffect, useReducer } from "react";
 import '../Style/StoryCreate.css';
 import TitleInput from '../stories/TitleInput/TitleInput';
 import MarkDownEditorDisplay from '../stories/MarkDownEditorDisplay/MarkDownEditorDisplay'
@@ -6,10 +6,9 @@ import Button from "../stories/Button/Button";
 
 const initialState = {
     title: '',
-    markdown:'# Heading',
-    titleError:'',
-    MarkDownError:'',
-    
+    markdown: '# Heading',
+    titleError: '',
+    MarkDownError: '',
 }
 
 const reducer = (state = initialState, action) => {
@@ -17,104 +16,155 @@ const reducer = (state = initialState, action) => {
         case 'TITLE_DATA': {
             return { ...state, title: action.payload }
         }
-        case 'MARKDOWN_DATA':{
-            return {...state, markdown:action.payload}
+        case 'MARKDOWN_DATA': {
+            return { ...state, markdown: action.payload }
         }
-        case 'ERROR_TITLE':{
-            return {...state,titleError:action.payload}
+        case 'ERROR_TITLE': {
+            return { ...state, titleError: action.payload }
         }
-        case 'ERROR_MARKDOWN':{
-            return {...state,MarkDownError:action.payload}
+        case 'ERROR_MARKDOWN': {
+            return { ...state, MarkDownError: action.payload }
         }
-        case 'PUBLISH_DATA':{
-            (localStorage.getItem('markDown')===null)?localStorage.setItem('markDown',JSON.stringify([])):null
-            const publishedData=JSON.parse(localStorage.getItem('markDown'))
-            localStorage.setItem('markDown',JSON.stringify([action.payload,...publishedData]))
-            return {...state,title: '',markdown:'# Heading',titleError:'',MarkDownError:'',}
+        case 'PUBLISH_DATA': {
+            localStorage.getItem('markDown') === null ? localStorage.setItem('markDown', JSON.stringify([])) : null
+            const publishedData = JSON.parse(localStorage.getItem('markDown'))
+            localStorage.setItem('markDown', JSON.stringify([{id:action.payload.id, title: action.payload.title, markDownData: action.payload.markDownData} , ...publishedData]))
+            action.payload.history.push('/published')
+            return { ...state, title: '', markdown: '# Heading', titleError: '', MarkDownError: ''}
+        }
+        case 'SET_EDIT_DATA': {
+            return { ...state, title: action.payload.title, markdown: action.payload.markDownData }
+        }
+        case 'REPUBLISH_DATA': {
+            const rePublish = JSON.parse(localStorage.getItem('markDown')).map((e) => {
+                if (e.id === action.payload.id) {
+                    return { id: e.id, title: action.payload.title, markDownData: action.payload.markDownData }
+                } else {
+                    return e
+                }
+            })
+            localStorage.setItem('markDown', JSON.stringify(rePublish))
+            action.payload.history.push('/published')
+            return { ...state, title: '', markdown: '# Heading', titleError: '', MarkDownError: '', }
         }
         default: {
             return { ...state }
         }
     }
 }
+
 const StoryCreate = (props) => {
     const [data, dispatch] = useReducer(reducer, initialState)
+    const editData = props.location.state
 
-    const handelSearch = (e) => {
+    useEffect(() => {
+        if (editData) {
+            dispatch({
+                type: 'SET_EDIT_DATA',
+                payload: editData
+            })
+        }
+    }, [editData])
+
+    const handelTitle = (e) => {
         dispatch({
             type: 'TITLE_DATA',
             payload: e.target.value
         })
     }
 
-    const handelMarkdownValue=(e)=>{
+    const handelMarkdownValue = (e) => {
         dispatch({
-            type:'MARKDOWN_DATA',
-            payload:e.target.value
+            type: 'MARKDOWN_DATA',
+            payload: e.target.value
         })
     }
 
-    const handelPublish=(e)=>{
+    const handelPublish = (e) => {
         e.preventDefault()
-        let errorStatus=false
-        let existData=JSON.parse(localStorage.getItem('markDown'))!==null && JSON.parse(localStorage.getItem('markDown')).filter((e)=>{
-            return e.title===data.title
+        let errorStatus = false
+        let existData = JSON.parse(localStorage.getItem('markDown')) !== null && JSON.parse(localStorage.getItem('markDown')).filter((e) => {
+            return e.title === data.title
         })
-
-        if(data.title.length===0){
-            errorStatus=true
+        if (data.title.length === 0) {
+            errorStatus = true
             dispatch({
-                type:'ERROR_TITLE',
-                payload:'Title Is Empty Pls Enter Title'
+                type: 'ERROR_TITLE',
+                payload: 'Title Is Empty Pls Enter Title'
             })
-        }else if(data.title.length<3){
-            errorStatus=true
+        } else if (data.title.length < 3) {
+            errorStatus = true
             dispatch({
-                type:'ERROR_TITLE',
-                payload:'Title Is Too Short'
+                type: 'ERROR_TITLE',
+                payload: 'Title Is Too Short'
             })
-        }else if(data.title.length>50){
-            errorStatus=true
+        } else if (data.title.length > 50) {
+            errorStatus = true
             dispatch({
-                type:'ERROR_TITLE',
-                payload:'Title Is Too Lengthy'
+                type: 'ERROR_TITLE',
+                payload: 'Title Is Too Lengthy'
             })
-        } else if(existData.length>0){
-            errorStatus=true
-            dispatch({
-                type:'ERROR_TITLE',
-                payload:'Title already exist'
-            })
-        }
-
-        if(data.markdown.length===0){
-            errorStatus=true
-            dispatch({
-                type:'ERROR_MARKDOWN',
-                payload:'Input is Empty '
-            })
-        }
-
-        !errorStatus&& dispatch({
-            type:'PUBLISH_DATA',
-            payload:{
-                id:Date.now(),
-                title:data.title,
-                markDownData:data.markdown
+        } else if (existData.length > 0) {
+            if (editData) {
+                if (existData[0].id !== editData.id) {
+                    errorStatus = true
+                    dispatch({
+                        type: 'ERROR_TITLE',
+                        payload: 'Title already exist'
+                    })
+                }
+            } else {
+                errorStatus = true
+                dispatch({
+                    type: 'ERROR_TITLE',
+                    payload: 'Title already exist'
+                })
             }
-        })
+        }
+
+        if (data.markdown.length === 0) {
+            errorStatus = true
+            dispatch({
+                type: 'ERROR_MARKDOWN',
+                payload: 'Input is Empty '
+            })
+        }
+
+        if (!errorStatus) {
+            if (editData) {
+                dispatch({
+                    type: 'REPUBLISH_DATA',
+                    payload: {
+                        id: editData.id,
+                        title: data.title,
+                        markDownData: data.markdown,
+                        history:props.history
+                    }
+                })
+            } else {
+                dispatch({
+                    type: 'PUBLISH_DATA',
+                    payload: {
+                        id: Date.now(),
+                        title: data.title,
+                        markDownData: data.markdown,
+                        history:props.history
+                    }
+                })
+            }   
+        }
     }
 
     return (
         <div>
             <div className="inputCont">
-                <TitleInput size='medium' handelChange={(e) => { handelSearch(e) }} placeHolder='Enter Title' value={data.title} error={data.titleError} type={'text'} />
+                <TitleInput size='medium' handelChange={(e) => { handelTitle(e) }} placeHolder='Enter Title' value={data.title} error={data.titleError} type={'text'} />
             </div>
             <div className="textCont">
-                <MarkDownEditorDisplay size='large' handelChange={(e)=>{handelMarkdownValue(e)}} markDown={data.markdown} error={data.MarkDownError} type={'text'}/>
+                <MarkDownEditorDisplay size='large' handelChange={(e) => { handelMarkdownValue(e) }} markDown={data.markdown} error={data.MarkDownError} type={'text'} />
             </div>
             <div className="ButtonCont">
-                <Button variant={`primary`} children={`Publish`} handelClick={handelPublish}/>
+                <Button variant={`primary`} children={`Publish`} handelClick={handelPublish} />
             </div>
         </div>
     )
--- src/Components/StoryPublish.js
+++ src/Components/StoryPublish.js
@@ -1,14 +1,14 @@
-import React, { useReducer,useEffect } from "react";
+import React, { useReducer,useEffect} from "react";
 import '../Style/StoryPublish.css'
 import MarkDownViewer from '../stories/MarkDownViewer/MarkDownViewer'
 
 const initialState = {
-  post: localStorage.getItem('markDown') === null ? [] : JSON.parse(localStorage.getItem('markDown')),
+  post: localStorage.getItem('markDown') == null ? [] : JSON.parse(localStorage.getItem('markDown')),
 }
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'UPDATE_DELETE_DATA': {
+    case 'UPDATE_DATA': {
       return { ...state, post: action.payload }
     }
     default: {
@@ -19,33 +19,38 @@ const reducer = (state = initialState, action) => {
 const StoryPublish = (props) => {
   const [data, dispatch] = useReducer(reducer, initialState)
 
-  useEffect(()=>{
+  const updateData=()=>{
     dispatch({
-      type: 'UPDATE_DELETE_DATA',
-      payload: localStorage.getItem('markDown') === null ? [] : JSON.parse(localStorage.getItem('markDown'))
+      type: 'UPDATE_DATA',
+      payload:localStorage.getItem('markDown') === null ? [] : JSON.parse(localStorage.getItem('markDown'))
     })
-  },[localStorage.getItem('markDown') === null ? [] : JSON.parse(localStorage.getItem('markDown'))])
+  }
+  
+  useEffect(()=>{
+    updateData()
+  },[])
 
   const handelDelete = (id) => {
     const deleteData = data.post.filter((e) => {
       return e.id !== id
     })
     localStorage.setItem('markDown', JSON.stringify(deleteData))
+    updateData()
   }
 
-  const handelEdit = (e) => {
-    props.history.push({ pathname:'/post', state:e })
+  const handelEdit = (e,i) => {
+    props.history.push({ pathname:'/post', state:{...e,postion:i} })
   }
 
 
   return (
     <div className='markDownViewer'>
-      {(data.post.length <= 0) ? <p className="empty">No Post Available Pls Create Post</p> : data.post.map((e) => {
+      {(data.post.length <= 0) ? <p className="empty">No Post Available Pls Create Post</p> : data.post.map((e,i) => {
         return (
           <div key={e.id}>
             <div className="Edit-icon">
               <i className="bi bi-trash-fill icon" onClick={() => { handelDelete(e.id) }}></i>
-              <i className="bi bi-pencil-square icon" onClick={() => { handelEdit(e) }}></i>
+              <i className="bi bi-pencil-square icon" onClick={() => { handelEdit(e,i) }}></i>
             </div>
             <MarkDownViewer markDownTitle={e.title} markDownData={e.markDownData} />
           </div>
